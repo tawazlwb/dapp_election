70c2de8c999f4e0a63685b70f2ac9b34281b1de8
Ismail KHEIRY <ikheiry@sqli.com>
2019-04-07 21:39:26 +0200

    first commit

---
+++ test/help.js
@@ -0,0 +1,10 @@
+// *CMD*
+// truffle init
+// truffle unbox pet-shop
+// truffle migrate
+// truffle migrate --reset
+// truffle console
+
+/* Election.deployed().then(function(instance) {
+  app = instance;
+}); */
